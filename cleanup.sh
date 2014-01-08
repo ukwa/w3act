956754161a4b17ab42fe998533f755e7576bce05
GilHoggarth <gil.hoggarth@bl.uk>
2014-01-08 10:52:22 +0000

    Amendments to allow play start to run without postgres

---
+++ cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm -rf target/ project/target/ project/project/target/ conf/evolutions/
+echo Project generated target folders clean up completed. 
+
+# replace psql path, database name (-d), host (-h), port (-p) and user (-U) if needed
+#psql -d w3act -h localhost -p 5432 -U training -f cleanupDB.sql
+#echo PostgreSQL schema clean up completed. 
