06ca8bb466ca80324b5bdff57c71671563975a63
GilHoggarth <gil.hoggarth@bl.uk>
2014-01-10 16:09:52 +0000

    Merged bash cleanup scripts; removed extra file

--- cleanup.sh
+++ cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -rf target/ project/target/ project/project/target/ 
-echo Project generated target folders clean up completed. 
+rm -rf target/ project/target/ project/project/target/ conf/evolutions/
+echo Project generated folders clean up completed. 
 
 # replace psql path, database name (-d), host (-h), port (-p) and user (-U) if needed
 #psql -d w3act -h localhost -p 5432 -U training -f cleanupDB.sql
