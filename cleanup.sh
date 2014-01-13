26d1803170d7ddcdd2cb4519beeb72c88b8e3ed4
GilHoggarth <gil.hoggarth@bl.uk>
2014-01-13 15:45:32 +0000

    Removed dropdb, createdb, grant as unnecessary

--- cleanup.sh
+++ cleanup.sh
@@ -5,8 +5,7 @@ echo Project generated folders clean up completed.
 
 # postgres setup:
 # su - postgres -c "createuser --superuser --password training"
+#su - postgres -c "createdb --owner=training --username=training w3act"
+#su - postgres -c "psql -c 'grant all on database w3act to training'"
 psql -d w3act -U training -f cleanupDB.sql
-su - postgres -c "dropdb w3act"
-su - postgres -c "createdb --owner=training --username=training w3act"
-su - postgres -c "psql -c 'grant all on database w3act to training'"
 echo PostgreSQL schema clean up completed. 
