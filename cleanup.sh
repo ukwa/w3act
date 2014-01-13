25290e5d1677dc01974b6053cc7ec3c64fb7eaa2
GilHoggarth <gil.hoggarth@bl.uk>
2014-01-13 11:08:47 +0000

    Added previous createuser command, and changed cleanDB.sql user to training

--- cleanup.sh
+++ cleanup.sh
@@ -3,7 +3,9 @@
 rm -rf target/ project/target/ project/project/target/ conf/evolutions/
 echo Project generated folders clean up completed. 
 
-psql -d w3act -U postgres -f cleanDB.sql
+# postgres setup:
+# su - postgres -c "createuser --superuser --password training"
+psql -d w3act -U training -f cleanDB.sql
 su - postgres -c "dropdb w3act"
 su - postgres -c "createdb --owner=training --username=training w3act"
 su - postgres -c "psql -c 'grant all on database w3act to training'"
