ebeecff5b8c7276d96276c6862f1d09e90df2613
GilHoggarth <gil.hoggarth@bl.uk>
2014-01-13 11:41:52 +0000

    Correction to cleanupDB.sql filename

--- cleanup.sh
+++ cleanup.sh
@@ -5,7 +5,7 @@ echo Project generated folders clean up completed.
 
 # postgres setup:
 # su - postgres -c "createuser --superuser --password training"
-psql -d w3act -U training -f cleanDB.sql
+psql -d w3act -U training -f cleanupDB.sql
 su - postgres -c "dropdb w3act"
 su - postgres -c "createdb --owner=training --username=training w3act"
 su - postgres -c "psql -c 'grant all on database w3act to training'"
