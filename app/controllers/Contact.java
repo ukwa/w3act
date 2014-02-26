b771943c0a73ec916c95cdf612ad661633c8107e
rgraf <roman.graf@ait.ac.at>
2014-02-26 10:52:16 +0100

    + implemented Contact page.

--- app/controllers/Contact.java
+++ app/controllers/Contact.java
@@ -1,32 +1,19 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-import play.data.*;
-import static play.data.Form.*;
-
-import java.util.*;
-
-import models.*;
-
-import views.html.*;
-import uk.bl.api.*;
-import java.net.URL;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.lang.StringBuilder;
-import java.io.FileNotFoundException;
-import play.libs.Json;
+import models.User;
+import play.Logger;
+import play.mvc.Result;
+import play.mvc.Security;
 //import com.fasterxml.jackson.databind.JsonNode;
 import uk.bl.Const;
+import uk.bl.scope.EmailHelper;
+import views.html.contact;
 
 /**
  * Describe W3ACT project contacts.
  */
 @Security.Authenticated(Secured.class)
-public class Contact extends Controller {
+public class Contact extends AbstractController {
   
     /**
      * Display the About tab.
@@ -36,6 +23,25 @@ public class Contact extends Controller {
             contact.render("Contact", User.find.byId(request().username()))
         );
     }
+    
+    /**
+     * This method extracts form values and sends an email.
+     */
+    public static Result send() {
+		Logger.info("send contact request");
+        String name = getFormParam(Const.NAME);
+        String messageSubject = getFormParam(Const.SUBJECT);
+        String addMail = getFormParam(Const.FROM);
+        String toMail = Const.BL_MAIL_ADDRESS;
+        String content = getFormParam(Const.CONTENT);
+        String messageBody = "User name is " + name + "\n" + "User email is " + addMail + "\n" + content;
+        Logger.info("subject: " + messageSubject + ", to: " + toMail + ", messageBody: " + messageBody);
+        EmailHelper.sendMessage(toMail, messageSubject, messageBody);                	
+
+        Result res = redirect(routes.Contact.index()); 
+        return res;
+    }
+    
 	
 }
 
