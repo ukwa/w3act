19846b407860e93ac9ccdda01ffa6f1df6d4577c
rgraf <roman.graf@ait.ac.at>
2014-03-17 16:53:06 +0100

    + issue # 43: Github will not be accessible for last commit version.

--- app/controllers/About.java
+++ app/controllers/About.java
@@ -30,17 +30,17 @@ public class About extends Controller {
      */
     public static String getLastCommitHash() {
     	String row = "";
-    	try {
-    		row = Utils.buildWebRequestByUrl(Const.GITHUB, Const.LAST_COMMIT);
-        	Logger.info("row: " + row);
-	    	if (row != null && row.length() > 0) {
-		    	int start = row.indexOf(Const.LAST_COMMIT) + Const.LAST_COMMIT.length();
-		    	row = row.substring(start, start + 40);
-	    	}
-    	} catch (Exception e) {
-    		Logger.debug("Error occured by last commit hash calculation: " + e);
-    	}
-    	Logger.info("last commit hash: " + row);
+//    	try {
+//    		row = Utils.buildWebRequestByUrl(Const.GITHUB, Const.LAST_COMMIT);
+//        	Logger.info("row: " + row);
+//	    	if (row != null && row.length() > 0) {
+//		    	int start = row.indexOf(Const.LAST_COMMIT) + Const.LAST_COMMIT.length();
+//		    	row = row.substring(start, start + 40);
+//	    	}
+//    	} catch (Exception e) {
+//    		Logger.debug("Error occured by last commit hash calculation: " + e);
+//    	}
+//    	Logger.info("last commit hash: " + row);
     	return row;
     }
     
