4047426c4567550186ee2226904fc59347999f5e
kinmanli <kinman.li@bl.uk>
2014-06-10 16:57:49 +0100

    minor work to bat file

--- data_import.bat
+++ data_import.bat
@@ -2,19 +2,24 @@
 
 setLocal ENABLEDELAYEDEXPANSION
 
-SET PLAY_HOME=
+SET PLAY_HOME=c:\play-2.2.1
 
 IF "%PLAY_HOME%"=="" GOTO USAGE 
 
-SET REPO=%PLAY_HOME%\repository
+SET REPO=%PLAY_HOME%
+
+mkdir tmp
 
 if defined CLASSPATH (set CLASSPATH=%CLASSPATH%;.) else (set CLASSPATH=.)
-FOR /R %REPO% %%G IN (*.jar) DO set CLASSPATH=!CLASSPATH!;%%G
+
+rem FOR /R %REPO% %%G IN (*.jar) DO set CLASSPATH=!CLASSPATH!;%%G
+FOR /R %REPO% %%G IN (*.jar) DO xcopy /s/y %%G tmp
+
 echo The Classpath definition is %CLASSPATH%
 
 cd target\scala-2.10\classes
 
-java uk.bl.db.DataImport
+java -cp tmp\* uk.bl.db.DataImport
 
 GOTO END
 
