3e51e9236d30f1e4e7cc97ab9ed61abee13bb29f
kinmanli <kinman.li@bl.uk>
2014-11-07 15:23:56 +0000

    typo with sequence name

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -1,10 +1,10 @@
 # --- !Ups
 
-drop sequence if exists saved_search_seq;
+drop sequence if exists lookup_entry_seq;
 create sequence lookup_entry_seq start with 1;
 
 # --- !Downs
 
 create sequence lookup_entry_seq start;
-drop sequence if exists saved_search_seq;
+drop sequence if exists lookup_entry_seq;
 
