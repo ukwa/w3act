b98180fca7d8e553ee94cdeda3dccb5e4b50bb81
Roman Karl <roman.karl@ait.ac.at>
2014-11-27 16:55:32 +0100

    storing html files in assets

--- converter/convertPdfToHtml.sh
+++ converter/convertPdfToHtml.sh
@@ -7,6 +7,9 @@ wget -O $BASE_FILE_NAME.pdf $PDF_URL
 
 pdf2htmlEX --embed-font 0 --process-outline 0 $BASE_FILE_NAME.pdf
 
+rm $BASE_FILE_NAME.pdf
 rm *.woff
 
 sed -i s/@font-face{[^}]*}// $BASE_FILE_NAME.html
+
+mv $BASE_FILE_NAME.html ../app/assets/
