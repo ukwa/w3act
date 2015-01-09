5419ad9c37ef6059398992fbc09da400769b7725
Roman Karl <roman.karl@ait.ac.at>
2015-01-09 11:26:08 +0100

    handling of unsafe urls corrected

--- app/uk/bl/crawling/Crawler.java
+++ app/uk/bl/crawling/Crawler.java
@@ -2,7 +2,6 @@ package uk.bl.crawling;
 
 import java.io.IOException;
 import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -66,7 +65,7 @@ public class Crawler {
 							org.jsoup.nodes.Document doc = response.parse();
 							
 							for(Element element : doc.select("a[href]")) {
-								String waybackHrefUrl = URLEncoder.encode(element.absUrl("href"), "UTF-8");
+								String waybackHrefUrl = element.absUrl("href").replace(" ", "%20");
 								String hrefUrl = crawlWayback ?
 										urlFromWayback(waybackHrefUrl) : waybackHrefUrl;
 								if (hrefUrl != null && !knownSites.contains(hrefUrl)) {
