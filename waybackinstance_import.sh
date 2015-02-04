6645512e732b03825bbaa6e6c4260c1f9053449f
GilHoggarth <gil.hoggarth@bl.uk>
2015-02-04 11:37:23 +0000

    Added end slash to PLAY_HOME and full paths to CLASSPATH directories

--- waybackinstance_import.sh
+++ waybackinstance_import.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PLAY_HOME=/opt/play
+PLAY_HOME=/opt/play/
 
 if [ -z "$PLAY_HOME" ];then
     echo "PLAY_HOME NEEDS TO BE SET"
@@ -10,7 +10,8 @@ else
 	#CLASSPATH=$(find "${REPO}" -name *.jar -printf '%p:' | sed 's/:$//') 
 	CLASSPATH=$(find "${REPO}" -name '*.jar' | xargs echo | tr ' ' ':')
 
-	CLASSPATH=${CLASSPATH}:./target/scala-2.10/classes:./conf:./target/scala-2.10/classes_managed:./target/scala-2.10/src_managed:.
+	CLASSPATH=${CLASSPATH}:/opt/w3act/target/scala-2.10/classes:/opt/w3act/conf:/opt/w3act/target/scala-2.10/classes_managed:/opt/w3act/target/scala-2.10/src_managed:.
+#	CLASSPATH=${CLASSPATH}:./target/scala-2.10/classes:./conf:./target/scala-2.10/classes_managed:./target/scala-2.10/src_managed:.
 
 	export CLASSPATH
 
