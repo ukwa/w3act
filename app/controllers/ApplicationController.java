b55f816e86085d0c79358de5ff1558c5fed4f6c7
kinmanli <kinman.li@bl.uk>
2015-03-02 11:07:30 +0000

    updated the json input to test the endpoint. Also updated the 'mid-crawl' in instance list as it was pointing to opera. keysite was displaying 'yes' when it was false.

--- app/controllers/ApplicationController.java
+++ app/controllers/ApplicationController.java
@@ -3,6 +3,7 @@ package controllers;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
@@ -29,6 +30,7 @@ import uk.bl.api.models.FieldModel;
 import uk.bl.exception.ActException;
 import uk.bl.exception.TaxonomyNotFoundException;
 import uk.bl.exception.UrlInvalidException;
+import uk.bl.exception.WhoisException;
 import uk.bl.scope.Scope;
 import views.html.*;
 
@@ -204,10 +206,8 @@ public class ApplicationController extends Controller {
     /***
 	 *
 	 * 
-	 * {"body":[],"field_scope":"root","field_url":[{"url":"http:\/\/www.electionwatch.co.uk\/","attributes":[]}],"field_subject":{"uri":"http:\/\/webarchive.org.uk\/act\/taxonomy_term\/227","id":"227","resource":"taxonomy_term"},"field_depth":"capped","field_via_correspondence":false,"field_uk_postal_address":false,"field_uk_hosting":false,"field_description":[],"field_uk_postal_address_url":[],"field_nominating_organisation":{"uri":"http:\/\/webarchive.org.uk\/act\/node\/101","id":"101","resource":"node"},"field_crawl_frequency":"daily","field_suggested_collections":[],"field_collections":[],"field_crawl_start_date":"1422943200","field_crawl_end_date":"1431057600","field_uk_domain":"Yes","field_license":[],"field_crawl_permission":"","field_collection_categories":[{"uri":"http:\/\/webarchive.org.uk\/act\/taxonomy_term\/300","id":"300","resource":"taxonomy_term"},{"uri":"http:\/\/webarchive.org.uk\/act\/taxonomy_term\/305","id":"305","resource":"taxonomy_term"}],"field_special_dispensation":false,"field_special_dispensation_reaso":null,"field_live_site_status":null,"field_notes":[],"field_wct_id":null,"field_spt_id":null,"field_snapshots":[],"field_no_ld_criteria_met":false,"field_key_site":false,"field_uk_geoip":"Yes","field_professional_judgement":false,"field_professional_judgement_exp":null,"field_ignore_robots_txt":false,"field_instances":[],"nid":"21593","vid":"34746","is_new":false,"type":"url","title":"Election Watch - UK General Election 2015 ","language":"en","url":"http:\/\/webarchive.org.uk\/act\/node\/21593","edit_url":"http:\/\/webarchive.org.uk\/act\/node\/21593\/edit","status":"1","promote":"0","sticky":"0","created":"1422887984","changed":"1422887984","author":{"uri":"http:\/\/webarchive.org.uk\/act\/user\/61","id":"61","resource":"user"},"log":"","revision":null,"comment":"2","comments":[],"comment_count":"0","comment_count_new":"0","feed_nid":null}  
-	 *  
-	 * curl -v -H "Accept: application/json" -H "Content-Type: application/json" -X POST -u kinman.li@bl.uk:password -d '{"body":[],"field_scope":"root","field_url":[{"url":"http:\/\/www.electionwatch.co.uk\/","attributes":[]}],"field_subject":{"uri":"http:\/\/webarchive.org.uk\/act\/taxonomy_term\/227","id":"227","resource":"taxonomy_term"},"field_depth":"capped","field_via_correspondence":false,"field_uk_postal_address":false,"field_uk_hosting":false,"field_description":[],"field_uk_postal_address_url":[],"field_nominating_organisation":{"uri":"http:\/\/webarchive.org.uk\/act\/node\/101","id":"101","resource":"node"},"field_crawl_frequency":"daily","field_suggested_collections":[],"field_collections":[],"field_crawl_start_date":"1422943200","field_crawl_end_date":"1431057600","field_uk_domain":"Yes","field_license":[],"field_crawl_permission":"","field_collection_categories":[{"uri":"http:\/\/webarchive.org.uk\/act\/taxonomy_term\/300","id":"300","resource":"taxonomy_term"},{"uri":"http:\/\/webarchive.org.uk\/act\/taxonomy_term\/305","id":"305","resource":"taxonomy_term"}],"field_special_dispensation":false,"field_special_dispensation_reaso":null,"field_live_site_status":null,"field_notes":[],"field_wct_id":null,"field_spt_id":null,"field_snapshots":[],"field_no_ld_criteria_met":false,"field_key_site":false,"field_uk_geoip":"Yes","field_professional_judgement":false,"field_professional_judgement_exp":null,"field_ignore_robots_txt":false,"field_instances":[],"nid":"21593","vid":"34746","is_new":false,"type":"url","title":"Election Watch - UK General Election 2015 ","language":"en","url":"http:\/\/webarchive.org.uk\/act\/node\/21593","edit_url":"http:\/\/webarchive.org.uk\/act\/node\/21593\/edit","status":"1","promote":"0","sticky":"0","created":"1422887984","changed":"1422887984","author":{"uri":"http:\/\/webarchive.org.uk\/act\/user\/61","id":"61","resource":"user"},"log":"","revision":null,"comment":"2","comments":[],"comment_count":"0","comment_count_new":"0","feed_nid":null}' http://localhost:9000/actdev/api/targets
-	 * curl -v -H "Content-Type: application/json" -X POST -d '{"body":[],"field_scope":"root","field_url":[{"url":"http:\/\/www.electionwatch.co.uk\/","attributes":[]}],"field_subject":{"uri":"http:\/\/webarchive.org.uk\/act\/taxonomy_term\/227","id":"227","resource":"taxonomy_term"},"field_depth":"capped","field_via_correspondence":false,"field_uk_postal_address":false,"field_uk_hosting":false,"field_description":[],"field_uk_postal_address_url":[],"field_nominating_organisation":{"uri":"http:\/\/webarchive.org.uk\/act\/node\/101","id":"101","resource":"node"},"field_crawl_frequency":"daily","field_suggested_collections":[],"field_collections":[],"field_crawl_start_date":"1422943200","field_crawl_end_date":"1431057600","field_uk_domain":"Yes","field_license":[],"field_crawl_permission":"","field_collection_categories":[{"uri":"http:\/\/webarchive.org.uk\/act\/taxonomy_term\/300","id":"300","resource":"taxonomy_term"},{"uri":"http:\/\/webarchive.org.uk\/act\/taxonomy_term\/305","id":"305","resource":"taxonomy_term"}],"field_special_dispensation":false,"field_special_dispensation_reaso":null,"field_live_site_status":null,"field_notes":[],"field_wct_id":null,"field_spt_id":null,"field_snapshots":[],"field_no_ld_criteria_met":false,"field_key_site":false,"field_uk_geoip":"Yes","field_professional_judgement":false,"field_professional_judgement_exp":null,"field_ignore_robots_txt":false,"field_instances":[],"nid":"21593","vid":"34746","is_new":false,"type":"url","title":"Election Watch - UK General Election 2015 ","language":"en","url":"http:\/\/webarchive.org.uk\/act\/node\/21593","edit_url":"http:\/\/webarchive.org.uk\/act\/node\/21593\/edit","status":"1","promote":"0","sticky":"0","created":"1422887984","changed":"1422887984","author":{"uri":"http:\/\/webarchive.org.uk\/act\/user\/61","id":"61","resource":"user"},"log":"","revision":null,"comment":"2","comments":[],"comment_count":"0","comment_count_new":"0","feed_nid":null}' -u kinman.li@bl.uk:password http://localhost:9000/actdev/api/targets
+	 * {"title": "Your Thurrock","field_subject": {"resource": "taxonomy_term","uri": "http://webarchive.org.uk/act/taxonomy_term/192","id": "192"},"field_crawl_frequency": "monthly","field_nominating_organisation": {"resource": "node","uri": "http://www.webarchive.org.uk/act/node/101","id": "101"},"field_url": [{"url": "http://yourthurrock.com"}],"field_collection_categories": [{"resource": "taxonomy_term","uri": "http://www.webarchive.org.uk/act/taxonomy_term/297","id": "297"}],"type": "url","field_crawl_start_date": "1417255200"}  
+	 * curl -v -H "Content-Type: application/json" -X POST -d '{"title": "Your Thurrock","field_subject": {"resource": "taxonomy_term","uri": "http://webarchive.org.uk/act/taxonomy_term/192","id": "192"},"field_crawl_frequency": "monthly","field_nominating_organisation": {"resource": "node","uri": "http://www.webarchive.org.uk/act/node/101","id": "101"},"field_url": [{"url": "http://yourthurrock.com"}],"field_collection_categories": [{"resource": "taxonomy_term","uri": "http://www.webarchive.org.uk/act/taxonomy_term/297","id": "297"}],"type": "url","field_crawl_start_date": "1417255200"}' -u kinman.li@bl.uk:password http://localhost:9000/actdev/api/targets
      * @throws ActException 
 	 **/
     @With(SecuredAction.class)
@@ -228,41 +228,22 @@ public class ApplicationController extends Controller {
 				
 				Logger.debug("target: " + target);
 				
-				target.url = Utils.INSTANCE.getActUrl(target.getNid());
-				target.edit_url = Utils.INSTANCE.getWctUrl(target.vid);
-				target.createdAt = Utils.INSTANCE.getDateFromSeconds(target.getCreated());
-				FieldModel qaIssueField = target.getField_qa_status();
-				try {
-					if (qaIssueField != null) {
-						QaIssue qaIssue = getQaIssue(qaIssueField);
-						target.qaIssue = qaIssue;
-					}
-				} catch (TaxonomyNotFoundException tnfe) {
-					tnfe.printStackTrace();
-				}
-
-				// {"body":{"value":"<p>UK address on contacts page</p>\n", "summary":"", "format":"plain_text"},
-				if (target.getBody() instanceof LinkedHashMap) {
-					Map<String, String> bodyMap = (LinkedHashMap<String,String>)target.getBody();
-					String value = bodyMap.get("value");
-					String format = bodyMap.get("format");
-					String summary = bodyMap.get("summary");
-					if (StringUtils.isNotEmpty(value)) {
-						target.value = value;
-					}
-					if (StringUtils.isNotEmpty(format)) {
-						target.format = format;
-					}
-					if (StringUtils.isNotEmpty(summary)) {
-						target.summary = summary;
-					}
-				}
+//				{
+//					"title": "Your Thurrock", done
+//					"field_subject": {"resource": "taxonomy_term","uri": "http://webarchive.org.uk/act/taxonomy_term/192","id": "192"}, done
+//					"field_crawl_frequency": "monthly", done
+//					"field_nominating_organisation": {"resource": "node","uri": "http://www.webarchive.org.uk/act/node/101","id": "101"}, done
+//					"field_url": [{"url": "http://yourthurrock.com"}], done
+//					"field_collection_categories": [{"resource": "taxonomy_term","uri": "http://www.webarchive.org.uk/act/taxonomy_term/297","id": "297"}],
+//					"type": "url", done
+//					"field_crawl_start_date": "1417255200" done
+//				}
 				
-				// "field_url":[{"url":"http://www.childrenslegalcentre.com/"}],
 				List<FieldUrl> fieldUrls = new ArrayList<FieldUrl>();
 				for (Map<String,String> map : target.getField_url()) {
 					String url = map.get("url");
 					try {
+						// this take from old import as there were some dodgy URL's coming from old ACT
 						url = Utils.INSTANCE.validateUrl(url);
 						Logger.debug("Checked Url: " + url);
 						
@@ -280,35 +261,6 @@ public class ApplicationController extends Controller {
 					}
 				}
 				
-				if (!fieldUrls.isEmpty()) {
-					target.fieldUrls = fieldUrls;
-//					As for the multiple URL case, the logic should be that there is only one 
-//					"UK Hosting", 
-//					"UK top-level domain", 
-//					"UK Registration" 
-//					field, but all URLs have to meet it to get a tick.
-//					e.g. if all URLs end '.uk' then "UK top-level domain" is true.
-//					field_uk_hosting
-//					target.isUkHosting = Scope.INSTANCE.isUkHosting(fieldUrls); // check if UK IP Address
-//					field_uk_domain
-//					target.isTopLevelDomain = Scope.INSTANCE.isTopLevelDomain(fieldUrls);
-//					field_uk_geoip
-//					target.isUkRegistration = Scope.INSTANCE.isUkRegistration(fieldUrls);
-//					Logger.debug(target.isUkHosting + ", " + target.isTopLevelDomain + ", " + target.isUkRegistration);
-					// .uk .london .scot
-				}
-
-				// uk hosting
-				target.isUkHosting = BooleanUtils.toBoolean(target.getField_uk_geoip());
-				
-				// top level domain
-				target.isTopLevelDomain = BooleanUtils.toBoolean(target.getField_uk_domain());
-
-				target.isInScopeIp = false;
-				target.isInScopeIpWithoutLicense = false;
-
-				
-				// "field_subject":{"uri":"http://www.webarchive.org.uk/act/taxonomy_term/10","id":"10","resource":"taxonomy_term"},
 				// "field_subject":{"uri":"http://www.webarchive.org.uk/act/taxonomy_term/16","id":"16","resource":"taxonomy_term"},"
 				FieldModel fieldSubject = target.getField_subject();
 				if (fieldSubject != null) {
@@ -316,33 +268,17 @@ public class ApplicationController extends Controller {
 						Subject subject = getSubject(fieldSubject);
 						target.subjects.add(subject);
 					} catch (TaxonomyNotFoundException tnfe) {
-						tnfe.printStackTrace();
+						throw new ActException(tnfe);
 					}
 				}
-				
-				// "field_description":[],
-				if (target.getField_description() != null && target.getField_description() instanceof Map) {
-					Map<String, String> fieldDescription = (Map<String, String>)target.getField_description();
-					String value = fieldDescription.get("value");
-					String summary = fieldDescription.get("summary");
-					if (StringUtils.isNotEmpty(value)) {
-						target.description = value;
-					}
-					
-					if (StringUtils.isNotEmpty(summary)) {
-						// populate if not already filled in by body->value
-						if (StringUtils.isEmpty(target.summary)) {
-							target.summary = summary;
-						}
-					}
+
+				// "field_url":[{"url":"http://www.childrenslegalcentre.com/"}],
+				if (!fieldUrls.isEmpty()) {
+					target.fieldUrls = fieldUrls;
 				}
-				
-				// "field_uk_postal_address_url":{"url":"http://www.childrenslegalcentre.com/index.php?page=contact_us"},
-				if (target.getField_uk_postal_address_url() != null && target.getField_uk_postal_address_url() instanceof Map) {
-					Map<String,String> postalAddressUrl = (Map<String,String>)target.getField_uk_postal_address_url();
-					String url = postalAddressUrl.get("url");
-					target.ukPostalAddressUrl = url;
-				}
+
+				// "field_crawl_frequency": "monthly"
+				target.crawlFrequency = target.crawlFrequency.toUpperCase();
 
 				// "field_nominating_organisation":{"uri":"http://www.webarchive.org.uk/act/node/101","id":"101","resource":"node"},
 				FieldModel fieldNominatingOrganisation = target.getField_nominating_organisation();
@@ -352,103 +288,50 @@ public class ApplicationController extends Controller {
 						target.organisation = Organisation.findByUrl(orgUrl);
 					}
 				}
-
-				// "field_license":[{"uri":"http://www.webarchive.org.uk/act/taxonomy_term/168","id":"168","resource":"taxonomy_term"}],
-				if (target.getField_license() != null) {
-					List<FieldModel> licenses = target.getField_license();
-					for (FieldModel fieldModel : licenses) {
-						try {
-							License license = getLicense(fieldModel);
-							if (license != null) {
-								target.licenses.add(license);
-							}
-						} catch (TaxonomyNotFoundException tnfe) {
-							tnfe.printStackTrace();
-						}
-					}
-				}
 				
 				// "field_collection_categories":[{"uri":"http://www.webarchive.org.uk/act/taxonomy_term/160","id":"160","resource":"taxonomy_term"}],
 				if (target.getField_collection_categories() != null) {
 					List<FieldModel> collectionCategories = target.getField_collection_categories();
 					
 					for (FieldModel fieldModel : collectionCategories) {
-						try {
-							Collection collection = getCollection(fieldModel);
-							if (collection != null) {
-								target.collections.add(collection);
-							}
-						} catch (TaxonomyNotFoundException tnfe) {
-							tnfe.printStackTrace();
+						Collection collection = getCollection(fieldModel);
+						if (collection != null) {
+							target.collections.add(collection);
 						}
 					}
 				}
-				
-				// "field_crawl_start_date":null,
+
+				// "field_crawl_start_date": "1417255200"
 				if (target.getField_crawl_start_date() != null) {
 					target.crawlStartDate = Utils.INSTANCE.getDateFromSeconds(target.getField_crawl_start_date());
 				}
-				// "field_crawl_end_date":null,
-				if (target.getField_crawl_end_date() != null) {
-					target.crawlEndDate = Utils.INSTANCE.getDateFromSeconds(target.getField_crawl_end_date());
-				}
-				
-				// "field_notes":{"value":"<p>There are missing documents in this gather for example <a href=\"https://www.wjec.co.uk/uploads/publications/16330.pdf\">https://www.wjec.co.uk/uploads/publications/16330.pdf</a>. It could be a capping issue. I have taken off the cap.</p>\n","format":"filtered_html"}
-				if (target.getField_notes() != null && target.getField_notes() instanceof Map) {
-					Map<String,String> fieldNotes = (Map<String,String>)target.getField_notes();
-					String value = fieldNotes.get("value");
-					target.notes = value;
-				}
-				
-				// "field_snapshots":[{"uri":"http://www.webarchive.org.uk/act/field_collection_item/6","id":"6","resource":"field_collection_item"}]
-				if (target.getField_snapshots() != null) {
-					List<FieldModel> fieldSnapshots = target.getField_snapshots();
-					List<String> fieldSnapshotsList = new ArrayList<String>();
-					for (FieldModel fieldModel : fieldSnapshots) {
-						String actUrl = Utils.INSTANCE.getActUrl(fieldModel.getId());
-						fieldSnapshotsList.add(actUrl);
-					}
 
-				}
+				target.url = "act-" + Utils.INSTANCE.createId();
 				
-				// "field_instances":[],
-				target.getField_instances();
-
-				// "author":{"uri":"http://www.webarchive.org.uk/act/user/9","id":"9","resource":"user"},
-				FieldModel author = target.getAuthor();
-				if (author != null) {
-					User authorUser = User.findByUrl(Utils.INSTANCE.getActUrl(author.getId()));
-					target.authorUser = authorUser;
-				}
-
+				target.isUkHosting = target.isUkHosting();
+				target.isTopLevelDomain = target.isTopLevelDomain();
+				target.isUkRegistration = target.isUkRegistration();
+				
+//				target.edit_url = Utils.INSTANCE.getWctUrl(target.vid);
+//				target.createdAt = Utils.INSTANCE.getDateFromSeconds(target.getCreated());
+				
 				target.revision = Const.INITIAL_REVISION;
 				target.active = true;
+				
 				target.selectionType = Const.SelectionType.SELECTION.name();
-				if (StringUtils.isNotBlank(target.language) && target.language.equals(Const.UND)) {
-					target.language = null;
-				} else {
-					target.language = target.language.toUpperCase();
-				}
-
+				
 				if (target.noLdCriteriaMet == null) {
-					target.noLdCriteriaMet = false;
+					target.noLdCriteriaMet = Boolean.FALSE;
 				}
 
 				if (target.keySite == null) {
-					target.keySite = false;
+					target.keySite = Boolean.FALSE;
 				}
-				
+
 				if (target.ignoreRobotsTxt == null) {
-					target.ignoreRobotsTxt = false;
+					target.ignoreRobotsTxt = Boolean.FALSE;
 				}
-				
-				if (StringUtils.isNotEmpty(target.liveSiteStatus)) {
-					target.liveSiteStatus = target.liveSiteStatus.toUpperCase();
-				}
-				
-				target.crawlFrequency = target.crawlFrequency.toUpperCase();
-				target.depth = target.depth.toUpperCase();
-				
+
 	        	target.save();
 				
 				Logger.debug("target: " + target);
@@ -459,7 +342,7 @@ public class ApplicationController extends Controller {
 				Logger.debug("response 201 created");
 			    return created(response().getHeaders().get(LOCATION));
 	    	}
-        } catch (IOException e) {
+        } catch (IOException | WhoisException | URISyntaxException | TaxonomyNotFoundException e) {
             return Results.internalServerError();
         }
 	}
