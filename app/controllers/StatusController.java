9e6682999dba38ebcf3fe68ea3f08846d564aa09
Roman Karl <roman.karl@ait.ac.at>
2015-05-11 14:16:10 +0200

    status page extended

--- app/controllers/StatusController.java
+++ app/controllers/StatusController.java
@@ -1,7 +1,12 @@
 package controllers;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import models.FastSubject;
 import models.User;
+import play.Play;
 import play.mvc.Result;
 import play.mvc.Security;
 import views.html.status.status;
@@ -39,6 +44,27 @@ public class StatusController extends AbstractController {
 			}
 		} catch (Exception e) {}
 		return false;
+	}
+	
+	public static boolean isWaybackResponding() {
+		return isSiteResponding(Play.application().configuration().getString("wayback_url"));
+	}
+	
+	public static boolean isPiiResponding() {
+		String arkRequest = Play.application().configuration().getString("pii_url");
+		String statusRequest = arkRequest.substring(0, arkRequest.lastIndexOf('/') + 1) + "status";
+		return isSiteResponding(statusRequest);
+	}
+	
+	public static boolean isSiteResponding(String url) {
+		try {
+			URL obj = new URL(url);
+			HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
+			connection.setRequestMethod("HEAD");
+			return connection.getResponseCode() == 200;
+		} catch (IOException e) {
+			return false;
+		}
 	}
 	
 	public static String secretServerVersion() {
--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -4,7 +4,7 @@ create table watched_target (
   id			bigint primary key,
   id_target		bigint not null references target (id),
   document_url_scheme	varchar(255),
-  wayback_timestamp	varchar(255),
+  wayback_timestamp	char(14),
   login_page_url	varchar(255),
   logout_url		varchar(255),
   secret_id		integer
