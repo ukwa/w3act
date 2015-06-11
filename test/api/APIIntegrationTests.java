850a4d0d255ed954339068a86010e1d0393c099e
Andrew Jackson <Andrew.Jackson@bl.uk>
2015-06-11 13:13:33 +0100

    Added first actual API test.

--- test/api/APIIntegrationTests.java
+++ test/api/APIIntegrationTests.java
@@ -12,13 +12,17 @@ import com.typesafe.config.ConfigFactory;
 
 import play.Configuration;
 import play.libs.F.Callback;
+import play.libs.F.Promise;
 import play.libs.WS;
 import play.test.*;
 import static play.test.Helpers.*;
 
 public class APIIntegrationTests {
 
-    private long timeout_ms = 10*1000; // in milliseconds
+    private static String defaultUser = "wa-sysadm@bl.uk";
+	private static String defaultPw = "sysAdmin";
+	
+	private static long timeout_ms = 20*1000; // in milliseconds
 	private Configuration additionalConfigurations;
 
 	@Before
@@ -34,6 +38,8 @@ public class APIIntegrationTests {
                 assertThat(
                     WS.url("http://localhost:3333/act").get().get(timeout_ms).getStatus()
                 ).isEqualTo(OK);
+                // Send up test data:
+            	sendTestData("http://localhost:3333/act");
             }
         });
     }
@@ -56,5 +62,32 @@ public class APIIntegrationTests {
                assertThat(browser.$("#title", 0).getText()).isEqualTo("Hello Coco");
             }
         });
+    }
+    
+    /*
+     * Method to populate a running system with some test data.
+     */
+    private static void populate(String host, String username, String password, String title, String url, String scope, String start_date, int expected) {
+    	String one = "{\"title\": \""+title+"\", \"field_urls\": [\""+url+"\"],\"field_scope\": \""+scope+"\",\"field_crawl_start_date\": \""+start_date+"\"}";
+    	Promise<WS.Response> result = WS.url(host+"/api/targets").setHeader("Content-Type", "application/json").setAuth(username, password).post(one);
+    	WS.Response response = result.get(timeout_ms);
+    	System.out.println("GOT "+response.getStatus()+" "+response.getStatusText());
+    	assertThat(response.getStatus()).isEqualTo(expected);
+    }
+    
+    private static void sendTestData(String host) {
+    	populate(host, defaultUser, defaultPw, "anjackson.net", "http://anjackson.net/","root", "", 201 );
+    	populate(host, defaultUser, defaultPw, "anjackson.net news", "http://anjackson.net/news/","resource", "1425790800", 201 );
+    	populate(host, defaultUser, defaultPw, "British Library", "http://www.bl.uk","root", "1425790800", 201 );
+    	populate(host, defaultUser, defaultPw, "British Library News", "http://www.bl.uk/news/","plus1", "1425790800", 201 );
+    }
+    
+    /**
+     * To populate a specified system with test data.
+     * 
+     * @param args
+     */
+    public static void main( String args[] ) {
+    	sendTestData("http://localhost:9000/act");
     }
 }
