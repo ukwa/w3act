0eecfa84bd1e8e22d92e308afcfbcda306017fa6
Niladree <niladree.bhattacharj@bl.uk>
2015-07-30 18:51:47 +0100

    3.sql edited for bug #372

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -3,6 +3,7 @@
 ALTER TABLE crawl_permission ADD granted_at timestamp;
 ALTER TABLE crawl_permission ADD requested_at timestamp;
 ALTER TABLE creator ADD last_login timestamp;
+ALTER TABLE contact_person ALTER COLUMN email SET DATA TYPE varchar(255);
 ALTER TABLE contact_person ADD CONSTRAINT uq_contact_person_email UNIQUE (email);
 
 # --- !Downs
@@ -11,3 +12,4 @@ ALTER TABLE crawl_permission DROP granted_at;
 ALTER TABLE crawl_permission DROP requested_at;
 ALTER TABLE creator DROP last_login;
 ALTER TABLE contact_person DROP CONSTRAINT uq_contact_person_email;
+ALTER TABLE contact_person ALTER COLUMN email SET DATA TYPE text;
