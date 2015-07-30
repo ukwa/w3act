0f4c8881c4bbbfb54f3173b69cdac6bd30edfd38
Niladree <niladree.bhattacharj@bl.uk>
2015-07-30 18:31:33 +0100

    #372 is resolved

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -1,11 +1,13 @@
 # --- !Ups
 
-alter table crawl_permission add granted_at timestamp;
-alter table crawl_permission add requested_at timestamp;
-alter table creator add last_login timestamp;
+ALTER TABLE crawl_permission ADD granted_at timestamp;
+ALTER TABLE crawl_permission ADD requested_at timestamp;
+ALTER TABLE creator ADD last_login timestamp;
+ALTER TABLE contact_person ADD CONSTRAINT uq_contact_person_email UNIQUE (email);
 
 # --- !Downs
 
-alter table crawl_permission drop granted_at;
-alter table crawl_permission drop requested_at;
-alter table creator drop last_login;
+ALTER TABLE crawl_permission DROP granted_at;
+ALTER TABLE crawl_permission DROP requested_at;
+ALTER TABLE creator DROP last_login;
+ALTER TABLE contact_person DROP CONSTRAINT uq_contact_person_email;
