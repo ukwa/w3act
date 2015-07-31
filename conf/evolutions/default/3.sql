6b16c17c59118a985e7c22656db1047ba011e8cf
Niladree <niladree.bhattacharj@bl.uk>
2015-07-31 11:16:17 +0100

    #310 resolved. New flag added to db

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -6,6 +6,10 @@ ALTER TABLE creator ADD last_login timestamp;
 ALTER TABLE contact_person ALTER COLUMN email SET DATA TYPE varchar(255);
 ALTER TABLE contact_person ADD CONSTRAINT uq_contact_person_email UNIQUE (email);
 
+INSERT INTO taxonomy (ttype, id, url, created_at, name, description, updated_at)
+    VALUES ('flags', 369, 'act-369', '2015-07-31 10:29:13.714', 'Video Content to add', 'This flag marks video content to add', '2015-07-31 10:29:13.714');
+
+
 # --- !Downs
 
 ALTER TABLE crawl_permission DROP granted_at;
@@ -13,3 +17,5 @@ ALTER TABLE crawl_permission DROP requested_at;
 ALTER TABLE creator DROP last_login;
 ALTER TABLE contact_person DROP CONSTRAINT uq_contact_person_email;
 ALTER TABLE contact_person ALTER COLUMN email SET DATA TYPE text;
+
+DELETE FROM taxonomy WHERE ttype = 'flags' AND name = 'Video Content to add';
