ccf501633cb784710e5185b1405aafe2bb195d5a
Niladree Bhattacharjee <Niladree.Bhattacharj@bl.uk>
2015-08-12 15:14:11 +0100

    Delete fixes.sql
    Purpose served.

--- fixes.sql
+++
@@ -1,5 +0,0 @@
-UPDATE crawl_permission SET contactperson_id = 825 WHERE contactperson_id = 824;
-DELETE FROM contact_person WHERE id = 824 AND email = 'info@ukjs.org';
-
-UPDATE crawl_permission SET contactperson_id = 1892 WHERE contactperson_id = 1891;
-DELETE FROM contact_person WHERE id = 1891 AND email = '    ds';
