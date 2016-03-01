62aa919775aabf19831601c9cb2fc87767fb859e
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-03-01 16:44:33 +0000

    Fixed document links and used buttons where appropriate.

--- app/controllers/DocumentSIPController.java
+++ app/controllers/DocumentSIPController.java
@@ -1,14 +1,18 @@
 package controllers;
 
+import org.apache.commons.lang3.StringUtils;
+
 import models.Document;
 import play.mvc.Result;
-
 import views.xml.documents.sip;
 
 public class DocumentSIPController extends AbstractController {
 		
 	public static Result sip(Long id) {
 		Document document = Document.find.byId(id);
+		if( StringUtils.isEmpty(document.ark) ) {
+			document.ark = "ark://PLACEHOLDER";
+		}
 		response().setContentType("text/xml");
 		return ok(sip.render(document));
 	}
