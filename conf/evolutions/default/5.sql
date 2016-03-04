7de2ed9b9b93f41a08c1ebf52b804fead8b10615
ldbiz <davey.business@gmail.com>
2016-03-04 14:20:25 +0000

    #490 New QA Status Added - data update changed to be run manually

--- conf/evolutions/default/5.sql
+++
@@ -1,9 +0,0 @@
-# --- !Ups
-
-INSERT INTO taxonomy (ttype, id, url, name, description, taxonomytype_id, created_at, updated_at)
-    VALUES ('quality_issues', nextval('taxonomy_seq'), 'act-201603041', 'QA issues, but OK to publish', '<p>QA issues, but OK to publish</p>', 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
-
-# --- !Downs
-
-DELETE FROM taxonomy WHERE url =  'act-201603041';
-
