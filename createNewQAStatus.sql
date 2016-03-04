f3def3e2421130cd1da58aca4acf404faef63656
ldbiz <davey.business@gmail.com>
2016-03-04 14:18:58 +0000

    #490 New QA Status Added - data change to manually run

---
+++ createNewQAStatus.sql
@@ -0,0 +1,7 @@
+
+INSERT INTO taxonomy (ttype, id, url, name, description, taxonomytype_id, created_at, updated_at)
+    VALUES ('quality_issues', nextval('taxonomy_seq'), 'act-201603041', 'QA issues, but OK to publish', '<p>QA issues, but OK to publish</p>', 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+	
+--To Remove	the new QA Status
+--DELETE FROM taxonomy WHERE url =  'act-201603041';
+
