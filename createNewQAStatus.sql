bac05123945271c1a0684502513b6240d032ee45
ldbiz <davey.business@gmail.com>
2016-03-04 14:31:44 +0000

    #490 New QA Status Added - data change

--- createNewQAStatus.sql
+++ createNewQAStatus.sql
@@ -1,6 +1,12 @@
 
+--Insert it if its not already there
 INSERT INTO taxonomy (ttype, id, url, name, description, taxonomytype_id, created_at, updated_at)
-    VALUES ('quality_issues', nextval('taxonomy_seq'), 'act-201603041', 'QA issues, but OK to publish', '<p>QA issues, but OK to publish</p>', 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+       SELECT 'quality_issues', nextval('taxonomy_seq'), 'act-201603041', 'QA issues, but OK to publish', '<p>QA issues, but OK to publish</p>', 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+       WHERE NOT EXISTS (SELECT 1 FROM taxonomy WHERE url='act-201603041');
+
+--Just insert it 
+--INSERT INTO taxonomy (ttype, id, url, name, description, taxonomytype_id, created_at, updated_at)
+--    VALUES ('quality_issues', nextval('taxonomy_seq'), 'act-201603041', 'QA issues, but OK to publish', '<p>QA issues, but OK to publish</p>', 4, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
 	
 --To Remove	the new QA Status
 --DELETE FROM taxonomy WHERE url =  'act-201603041';
