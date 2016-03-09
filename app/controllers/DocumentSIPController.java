0f33bc0d1a2c319a9639962ce9ab7f9524af31a0
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-03-09 14:16:42 +0000

    Modified SIP XML for documents based on feedback.

--- app/controllers/DocumentSIPController.java
+++ app/controllers/DocumentSIPController.java
@@ -11,13 +11,13 @@ public class DocumentSIPController extends AbstractController {
 	public static Result sip(Long id) {
 		Document document = Document.find.byId(id);
 		if( StringUtils.isEmpty(document.ark) ) {
-			document.ark = "ark://LOGICAL_ARK_PLACEHOLDER";
+			document.ark = "ark:/81055/vdc_100000000710.0x000001";
 		}
 		if( StringUtils.isEmpty(document.md_ark) ) {
-			document.md_ark = "ark://METADATA_ARK_PLACEHOLDER";
+			document.md_ark = "ark:/81055/vdc_100000000710.0x000002";
 		}
 		if( StringUtils.isEmpty(document.d_ark) ) {
-			document.d_ark = "ark://DATA_ARK_PLACEHOLDER";
+			document.d_ark = "ark:/81055/vdc_100000000710.0x000003";
 		}
 		response().setContentType("text/xml");
 		return ok(sip.render(document));
