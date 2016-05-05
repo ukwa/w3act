55b01decdcfcc893b1ed363f0fc5848455c642ab
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-05-05 13:59:26 +0100

    Add likely syntax to fix issue #508.

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -4,6 +4,12 @@ ALTER TABLE document ADD md_ark varchar(255);
 ALTER TABLE document ADD d_ark varchar(255);
 ALTER TABLE document ADD mets_d_ark varchar(255);
 
+# Likely syntax required to make sensible URL fields - Downs needed.
+# ALTER TABLE document ALTER COLUMN landing_page_url TYPE text;
+# ALTER TABLE document ALTER COLUMN document_url TYPE text;
+
+
+
 
 # --- !Downs
 
