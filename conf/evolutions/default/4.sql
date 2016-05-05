62228488363e2f9ab9e5f37fb2f051d5b6cbfdbd
Andrew Jackson <Andrew.Jackson@bl.uk>
2016-05-05 14:27:52 +0100

    Fix broken syntax by implementing rather than using comments.

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -4,9 +4,8 @@ ALTER TABLE document ADD md_ark varchar(255);
 ALTER TABLE document ADD d_ark varchar(255);
 ALTER TABLE document ADD mets_d_ark varchar(255);
 
-# Likely syntax required to make sensible URL fields - Downs needed.
-# ALTER TABLE document ALTER COLUMN landing_page_url TYPE text;
-# ALTER TABLE document ALTER COLUMN document_url TYPE text;
+ALTER TABLE document ALTER COLUMN landing_page_url TYPE text;
+ALTER TABLE document ALTER COLUMN document_url TYPE text;
 
 
 
@@ -17,3 +16,5 @@ ALTER TABLE document DROP md_ark;
 ALTER TABLE document DROP d_ark;
 ALTER TABLE document DROP mets_d_ark;
 
+ALTER TABLE document ALTER COLUMN landing_page_url TYPE varchar(255);
+ALTER TABLE document ALTER COLUMN document_url TYPE varchar(255);
