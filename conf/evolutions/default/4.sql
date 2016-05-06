37ceedd901d026966262ab38a132f75d4b84ba83
Chris Peacock <chris@chrispeacock.name>
2016-05-06 12:32:20 +0100

    #508 - Changed URL fields from varchar(255) to text type.

--- conf/evolutions/default/4.sql
+++ conf/evolutions/default/4.sql
@@ -4,9 +4,14 @@ ALTER TABLE document ADD md_ark varchar(255);
 ALTER TABLE document ADD d_ark varchar(255);
 ALTER TABLE document ADD mets_d_ark varchar(255);
 
-ALTER TABLE document ALTER COLUMN landing_page_url TYPE text;
-ALTER TABLE document ALTER COLUMN document_url TYPE text;
+ALTER TABLE document
+  ALTER COLUMN landing_page_url TYPE text,
+  ALTER COLUMN document_url TYPE text;
 
+ALTER TABLE watched_target
+  ALTER COLUMN document_url_scheme TYPE text,
+  ALTER COLUMN login_page_url TYPE text,
+  ALTER COLUMN logout_url TYPE text;
 
 
 
@@ -16,5 +21,11 @@ ALTER TABLE document DROP md_ark;
 ALTER TABLE document DROP d_ark;
 ALTER TABLE document DROP mets_d_ark;
 
-ALTER TABLE document ALTER COLUMN landing_page_url TYPE varchar(255);
-ALTER TABLE document ALTER COLUMN document_url TYPE varchar(255);
+ALTER TABLE document
+  ALTER COLUMN landing_page_url TYPE varchar(255),
+  ALTER COLUMN document_url TYPE varchar(255);
+
+ALTER TABLE watched_target
+  ALTER COLUMN document_url_scheme TYPE varchar(255),
+  ALTER COLUMN login_page_url TYPE varchar(255),
+  ALTER COLUMN logout_url TYPE varchar(255);
